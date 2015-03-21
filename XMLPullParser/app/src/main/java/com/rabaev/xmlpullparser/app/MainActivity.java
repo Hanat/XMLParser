89035e3f6bec99b99b395f422fe0d801e177ab7d
Sergey <fibritso@gmail.com>
2015-03-21 03:21:39 +0300

    fix

--- XMLPullParser/app/src/main/java/com/rabaev/xmlpullparser/app/MainActivity.java
+++ XMLPullParser/app/src/main/java/com/rabaev/xmlpullparser/app/MainActivity.java
@@ -1,7 +1,8 @@
 package com.rabaev.xmlpullparser.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -11,6 +12,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.IOException;
@@ -23,80 +25,88 @@ import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
 
-    ListView ecbList;
-    TextView messageText;
+  ListView ecbList;
+  TextView messageText;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        ecbList = (ListView) findViewById(R.id.ecbListView);
-        messageText = (TextView)findViewById(R.id.messageText);
+    ecbList = (ListView) findViewById(R.id.ecbListView);
+    messageText = (TextView) findViewById(R.id.messageText);
+  }
+
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if(id == R.id.action_settings) {
+      return true;
     }
 
+    return super.onOptionsItemSelected(item);
+  }
 
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+  public void getAdapter() {
+    final ArrayList<String> list = new ArrayList<String>();
+    new AsyncTask<String, Integer, InputStreamReader>() {
+      @Override
+      protected InputStreamReader doInBackground(String... url) {
+        try {
+          return new InputStreamReader(getUrlData(url[0]));
+        } catch (URISyntaxException e) {
+          messageText.setText(e.toString());
+          e.printStackTrace();
+        } catch (IOException e) {
+          messageText.setText(e.toString());
+          e.printStackTrace();
         }
+        return null;
+      }
 
-        return super.onOptionsItemSelected(item);
-    }
+      @Override
+      protected void onPostExecute(InputStreamReader result) {
+        try {
+          XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
 
-    public void getAdapter(){
-        final ArrayList<String> list = new ArrayList<String>();
-        try{
-            new AsyncTask<String, Integer, InputStreamReader>(){
-                protected Long doInBackground(String url) {
-                    return new InputStreamReader(getUrlData(url);
-                }
-
-                protected void onProgressUpdate(Integer progress) {
-                }
-
-                protected void onPostExecute(InputStreamReader result) {
-                    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-                    factory.setNamespaceAware(true);
-                    XmlPullParser parser = factory.newPullParser();
-                    parser.setInput(result);
-                    while (parser.getEventType() != XmlPullParser.END_DOCUMENT) {
-                        if (parser.getEventType() == XmlPullParser.START_TAG&& parser.getName().equals("Cube")) {
-                            list.add(parser.getAttributeValue(0) + " = "+ parser.getAttributeValue(1) + "\n");
-                        }
-                        parser.next();
-                    }
-                    ecbList.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, list););
-                }
-            }.execute("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
-
-        }catch (Throwable exc){
-            messageText.setText(exc.toString());
+          factory.setNamespaceAware(true);
+          XmlPullParser parser = factory.newPullParser();
+          parser.setInput(result);
+          while (parser.getEventType() != XmlPullParser.END_DOCUMENT) {
+            if(parser.getEventType() == XmlPullParser.START_TAG && parser.getName().equals("Cube")) {
+              list.add(parser.getAttributeValue(0) + " = " + parser.getAttributeValue(1) + "\n");
+            }
+            parser.next();
+          }
+        } catch (XmlPullParserException e) {
+          messageText.setText(e.toString());
+          e.printStackTrace();
+        } catch (IOException e) {
+          messageText.setText(e.toString());
+          e.printStackTrace();
         }
-    }
+        ecbList.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, list));
+      }
+    }.execute("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+  }
 
-    public InputStream getUrlData(String urlString)throws URISyntaxException, IOException {
-        DefaultHttpClient client = new DefaultHttpClient();
-        HttpPost method = new HttpPost(String.valueOf(new URL(urlString)));
-        HttpResponse res = client.execute(method);
-        return res.getEntity().getContent();
-    }
-
-
+  public InputStream getUrlData(String urlString) throws URISyntaxException, IOException {
+    DefaultHttpClient client = new DefaultHttpClient();
+    HttpPost method = new HttpPost(String.valueOf(new URL(urlString)));
+    HttpResponse res = client.execute(method);
+    return res.getEntity().getContent();
+  }
 }
